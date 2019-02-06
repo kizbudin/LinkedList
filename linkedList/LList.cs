e4d49eaa06603423d79fbb8a85b7efd085685fd6
M kizbudin <mkizbudin@gmail.com>
2019-02-06 12:24:15 +0700

    Add files via upload

---
+++ linkedList/LList.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linkedList
+{
+    public class LList
+    {
+        
+        public int[] Info;
+        public int[] Link;
+        public bool[] Flag;
+        public int Start, Avail;
+
+        
+        public LList()
+        {
+
+        }
+
+        
+        public LList(int Length, int iStart, int iAvail)
+        {
+            Info = new int[Length];
+            Link = new int[Length];
+            Flag = new bool[Length];
+            Start = iStart;
+            Avail = iAvail;
+        }
+
+
+        
+        public void Setup(int Fill, int Mulai, bool Mode)
+        {
+            int Ptr, Count;
+            Ptr = Mulai - 1; 
+            Count = 1; 
+
+            while (Count <= Fill)
+            {
+                Flag[Ptr] = true; 
+
+                if (Mode == false)
+                { 
+                    Console.Write("Info[{0}] :", Ptr + 1);
+                    Info[Ptr] = Convert.ToInt16(Console.ReadLine());
+                }
+
+                if (Count != Fill) 
+                {
+                    Console.Write("Link[{0}] :", Ptr + 1);
+                    Link[Ptr] = Convert.ToInt16(Console.ReadLine());
+                    while (Flag[Link[Ptr] - 1] == true) 
+                    {
+                        Console.WriteLine("Sudah Terisi!");
+                        Console.Write("Link[{0}] :", Ptr + 1);
+                        Link[Ptr] = Convert.ToInt16(Console.ReadLine());
+                    }
+                }
+                else 
+                {
+                    Link[Ptr] = 0;
+                }
+                Ptr = Link[Ptr] - 1;
+                Console.WriteLine("--------------------------");
+                Count++;
+            }
+            return;
+        }
+
+        public void InsFirst(int Item)
+        {
+            int New;
+            if (Avail == 0) Console.WriteLine("Overflow!");
+            New = Avail;
+            Avail = Link[Avail - 1];
+            Info[New - 1] = Item;
+            Link[New - 1] = Start;
+            Start = New;
+        }
+
+        public void Delete(int Item)
+        {
+            int Del;
+            if (Avail < 0) Console.Write("Underflow!");
+            Del = Item;
+            Item = Link[Item = 0];
+            Info[Del = 0] = Item;
+            Start = Del;
+        }
+
+        public void InsLoc(int Item, int Loc)
+        {
+            int New;
+            if (Avail == 0) Console.WriteLine("Overflow!");
+            else
+            {
+                New = Avail;
+                Avail = Link[Avail - 1];
+                Info[New - 1] = Item;
+                if (Loc == 0)
+                {
+                    Link[New - 1] = Start;
+                    Start = New;
+                }
+                else
+                {
+                    Link[New - 1] = Link[Loc - 1];
+                    Link[Loc - 1] = New;
+                }
+            }
+        }
+
+        public void ShowTable()
+        {
+            int iStart = Start - 1;
+            int iAvail = Avail - 1;
+            if (Start == 0)
+            {
+                Console.WriteLine("Output Tabel Linked List: ");
+                Console.WriteLine("");
+                Console.WriteLine("\t| Info \t| Link\t |");
+                Console.WriteLine("\t|--Null--|--Null--|");
+                Console.WriteLine("\t|--Null--|--Null--|");
+                Console.WriteLine("\t|--Null--|--Null--|");
+                Console.WriteLine("\t|--Null--|--Null--|");
+            }
+            else
+            {
+                Console.WriteLine("\t\t| Info\t| Link\t|");
+                Console.WriteLine("\t\t|-------|-------|");
+                for (int i = 0; i < Info.Length; i++)
+                {
+                    if (Flag[i] == false)
+                    {
+                        Console.WriteLine("[{0}]\t\t| Null\t| Null\t|", i + 1);
+                    }
+                    else if (i == iStart)
+                    {
+                        Console.WriteLine("[{0}]Start==>\t| {1}\t| {2}\t|", i + 1, Info[i], Link[i]);
+                    }
+                    else if (i == iAvail)
+                    {
+                        Console.WriteLine("[{0}]Avail-->\t| {1}\t| {2}\t|", i + 1, Info[i], Link[i]);
+                    }
+                    else Console.WriteLine("[{0}]\t\t| {1}\t| {2}\t|", i + 1, Info[i], Link[i]);
+
+                }
+                Console.WriteLine();
+            }
+        }
+
+
+        public void ShowLlist(int Mulai, string Label)
+        {
+            if (Mulai == 0)
+            {
+                Console.WriteLine("{0} :NULL", Label);
+            }
+            else
+            {
+                int Ptr;
+                Ptr = Mulai - 1;
+                Console.Write("{0} :", Label);
+                Console.Write("[{0}", Mulai);
+                while (Ptr != -1)
+                {
+                    Console.Write("]-[{0}|{1}", Info[Ptr], Link[Ptr]);
+                    Ptr = Link[Ptr] - 1;
+                }
+                Console.WriteLine("]");
+            }
+        }
+    }
+}
---
+++ linkedList/antarmukaInput.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linkedList
+{
+    class Interface
+    {
+        public LList p = new LList();
+
+        int Length, Fill, NotFill, Start, Avail;
+
+        public void MainMenu()
+        {
+            byte pil;
+            bool x = true;
+           
+            while (x == true)
+            {
+                Console.Clear();
+                Console.WriteLine("========================================================================================================================");
+                Console.WriteLine(String.Format("{0," + Console.WindowWidth / 1.5 + "}", "Kelompok Linked List 2IA03"));
+                Console.WriteLine("========================================================================================================================");
+                Console.WriteLine("");
+                p.ShowTable();
+                Console.WriteLine("");
+                p.ShowLlist(p.Start, "Start");
+                p.ShowLlist(p.Avail, "Avail");
+                Console.WriteLine("========================================================================================================================");
+                Console.WriteLine("Menu");
+                Console.WriteLine("1. Input Linked List\n2. Insert Linked List Dari Depan\n3. Insert Linked List Dari Tengah\n4. Keluar dari Program\n5. Delete");
+                Console.Write("Masukkan Pilihan :");
+                pil = Convert.ToByte(Console.ReadLine());
+                switch (pil)
+                {
+                    case 1:
+                        Reset();
+                        break;
+                    case 2:
+                        Insfirst();
+                        break;
+                    case 3:
+                        Insloc();
+                        break;
+                    case 4:
+                        x = false;
+                        break;
+                    case 5:
+                        Delete();
+                        break;
+
+                    default:
+                        Console.WriteLine("Masukan Pilihan!");
+                        Console.ReadKey();
+                        break;
+                }
+                
+            }
+        }
+
+        void Reset()
+        {
+            
+
+            Console.WriteLine("Masukkan Linked List");
+
+            Console.Write("Panjang Linked List:");
+            Length = Convert.ToInt16(Console.ReadLine());
+
+            Console.Write("Banyak Linked list yang ingin diisi : ");
+            Fill = Convert.ToInt16(Console.ReadLine());
+            NotFill = Length - Fill;
+
+            Console.Write("Start : ");
+            Start = Convert.ToInt16(Console.ReadLine());
+
+            Console.Write("Avail : ");
+            Avail = Convert.ToInt16(Console.ReadLine());
+
+            p = new LList(Length, Start, Avail);
+
+            
+
+            Console.WriteLine("===========================================");
+            Console.WriteLine("LINK");
+            p.Setup(Fill, Start, false);
+            Console.WriteLine("AVAIL");
+            p.Setup(NotFill, Avail, true);
+        }
+
+        void Insfirst()
+        {
+            int Item;
+            Console.WriteLine("Input Dari Awal");
+            Console.Write("Item baru :");
+            Item = Convert.ToInt16(Console.ReadLine());
+            p.InsFirst(Item);
+        }
+
+        void Insloc()
+        {
+            int Item, Loc;
+            Console.WriteLine("Input Dari Tengah");
+            Console.Write("Item baru :");
+            Item = Convert.ToInt16(Console.ReadLine());
+            Console.Write("Lokasi :");
+            Loc = Convert.ToInt16(Console.ReadLine());
+            p.InsLoc(Item, Loc);
+        }
+        void Delete()
+        {
+            int Item;
+            Console.WriteLine("Delete Linked List");
+            Console.WriteLine("Link");
+            Item = Convert.ToInt16(Console.ReadLine());
+            p.Delete(Item);
+        }
+            
+    }
+}
